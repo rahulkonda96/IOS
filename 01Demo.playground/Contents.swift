f4dca902655c174e3b9375043fed46de0d63c0d8
Rahul Konda <89164024+rahulkonda96@users.noreply.github.com>
2022-01-18 11:59:21 -0600

    Add files via upload

---
+++ 01Demo.playground/Contents.swift
@@ -0,0 +1,45 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+var name = "Konda"
+print("Hello \(name)!")
+
+var grade = "A"
+print("Hello, \(name) You got the \(grade) in IOS course")
+
+var age = 26;
+print("My age is \(age). When I triple my age, It would be \(age * 3) ")
+
+print("""
+      Hello
+      world!
+      """)
+
+print ("Hello All,\rWelcome to Swift programming")
+
+
+print("Welcome to Swift Programming")
+print("Fall 2021")
+print("*************")
+print("Welcome to Swift Programming" , terminator : "-" )
+print("Fall 2021")
+
+
+var name1 = "Konda"
+print("Hello \(name1)!")
+
+var grade1 = "A"
+print("Hello, \(name1) You got the \(grade1) in IOS course")
+
+print("Hello, \(name1)!" , terminator : "-")
+print("You got the \(grade1) in IOS course")
+
+
+print("The list of numbers are ")
+print(1,2,3,4,5,6)
+print("The new pattern is")
+print(1,2,3,4,5,6, separator: "-")
+
+print("The new pattern is", terminator: " ")
+print(1,2,3,4,5,6, separator: "-")
