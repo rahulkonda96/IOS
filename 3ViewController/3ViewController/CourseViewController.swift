c3ee727dd8cb050ea079dc61b547fa1bc4468400
Rahul Konda <89164024+rahulkonda96@users.noreply.github.com>
2022-03-24 12:16:43 -0500

    Add files via upload
    Multiple View Controller

---
+++ 3ViewController/3ViewController/CourseViewController.swift
@@ -0,0 +1,27 @@
+//
+//  CourseViewController.swift
+//  3ViewController
+//
+//  Created by Konda,Rahul on 3/24/22.
+//
+
+import UIKit
+
+class CourseViewController: UIViewController {
+
+    @IBOutlet weak var courseOutlet: UILabel!
+    
+    //This variable will hold the courses of the student,
+    var coursesArray:[Course] = []
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //when this screen loads we will loop through the courses and display them
+        for course in coursesArray {
+            courseOutlet.text = courseOutlet.text! + course.title + "-" + course.sem + "\n"
+        }
+       
+    }
+   
+    
+
+}
